3b80f65186539956d84c3b01cae2e7600cd700f3
DECREK <declanrek@gmail.com>
2013-07-10 16:58:39 +0200

    dashboard notification

--- assets/js/controllers.dashboard.js
+++ assets/js/controllers.dashboard.js
@@ -1,11 +1,18 @@
 'use strict';
 
 dcmsControllers.controller('DashboardCtrl', function DashboardCtrl($scope, Socket) {
+    $scope.messages = [];
+
 	$scope.sendMessage = function() {
 		Socket.send('aapje', {field1: 'aapje123', message: "Rofl!"});
 	};
 
 	Socket.on('aapje', function(data) {
 		console.log('Recieved an aapje', data);
+	});
+    Socket.on('documentsaved', function(data) {
+        $scope.$apply(function(){
+            $scope.messages.push({timestamp: data.data.timestamp, documentId: data.data.documentId, documentName:data.data.name});
+        });
 	});
 });
